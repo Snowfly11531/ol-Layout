63ce2b5fcc66c339d7bbfcae550955edadcfcf8a
hx_snowfly <245134041@qq.com>
2020-06-18 16:45:21 +0800

    first commit

--- dist/types/cesium.d.ts
+++
@@ -1,7 +0,0 @@
-import * as Cesium from 'cesium';
-declare class CesiumContainer {
-    cesiumViewer: Cesium.Viewer;
-    constructor();
-}
-export default CesiumContainer;
-//# sourceMappingURL=cesium.d.ts.map
